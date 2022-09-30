6e0605e0258bf0a247e2fed68ecb297b76e2acfb
Shenl <lshen30@jh.edu>
2022-09-30 17:16:56 -0400

    AAA

---
+++ utils.py
@@ -0,0 +1,115 @@
+import torch
+import numpy as np
+from transformers import GPT2LMHeadModel, GPT2Tokenizer, GPT2Model, GPT2Config
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+import os
+import json
+
+import random
+from tqdm import tqdm
+import argparse
+from torch.utils.data import Dataset
+import torch.nn.functional as F
+import torch.nn as nn
+null_words = ["N/A", "", "[MASK]"]
+num_top_tokens = 100
+num_gen_tokens = 1
+all_regular_preds = []
+all_calibrated_preds = []
+all_answers = []
+
+
+def train(train_loader, model, optimizer, EPOCHS):
+    model.train()
+    criterion = nn.CrossEntropyLoss()
+    for epoch_num in range(EPOCHS):
+        count = 0
+        total_loss_train = 0
+        for batch in train_loader:
+            sentence, label = batch
+            sentence['input_ids'] = sentence['input_ids'].squeeze(1).cuda()
+            sentence['attention_mask'] = sentence['attention_mask'].cuda()
+            ss = label['input_ids'].squeeze(1).cuda()
+            aa = ss[:,0]
+            logits = model(**sentence).logits[:, -1, :]
+            count += 1
+            loss = criterion(logits,aa.long())
+            optimizer.zero_grad()
+            loss.backward()
+            total_loss_train += loss
+            optimizer.step()
+
+        if epoch_num % 10 == 0:
+            print(total_loss_train/count)
+
+
+def validate(valloader, model):
+    model.eval()
+    with torch.no_grad():
+        total_sample = 0
+        correct = 0
+        for batch in valloader:
+            sentence, label = batch
+            sentence['input_ids'] = sentence['input_ids'].squeeze(1).cuda()
+            sentence['attention_mask'] = sentence['attention_mask'].cuda()
+            ss = label['input_ids'].squeeze(1)
+            logits = model(**sentence).logits[:, -1, :]
+            preds = torch.argmax(logits,dim=1)
+            labels = ss[:, 0]
+            correct += (np.array(preds.cpu()) ==  np.array(labels)).sum()
+            total_sample += preds.shape[0]
+        acc_total = correct/total_sample
+    return acc_total
+
+
+
+class loader_labeled(Dataset):
+    # Data loader for labeled data
+    def __init__(self, dataset_text, dataset_label, tokenizer, max_seq_len, instruction,model):
+        self.tokenizer = tokenizer
+        self.text = dataset_text
+        self.labels = dataset_label
+        self.max_seq_len = max_seq_len
+        self.model = model
+        self.ins = instruction
+        self.trans_dist = {}
+
+    def __len__(self):
+        return len(self.labels)
+
+
+    def __getitem__(self, idx):
+        if self.tokenizer.pad_token is None:
+            self.tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+            self.model.resize_token_embeddings(len(self.tokenizer))
+        encode_result = self.tokenizer(self.ins + '\n' + 'Input:' + self.text[idx] + '\n' + 'Output:', return_tensors='pt',padding='max_length',max_length=1000)
+        label = self.tokenizer(self.labels[idx], return_tensors='pt',padding='max_length',max_length=100)
+        return encode_result, label
+
+
+
+def lowercase_list(lst):
+    return [l.lower() for l in lst]
+
+
+def create_set(data,demo_num,seed,dict):
+    from random import sample
+    random.seed(seed)
+    test_inputs = [x['text'] for x in data['test']]
+    test_labels = [dict[str(x['label'])] for x in data['test']]
+
+    #demos = sample(data['train']['text'],demo_num)
+
+    demon_inputs = sample(data['train']['text'],demo_num)
+    demon_labels = sample(data['train']['label'],demo_num)
+
+    return demon_labels, demon_inputs, test_inputs, test_labels
+
+def pashuffle(string, perc=10):
+    data = string.split()
+    L = len(data)
+    gap = random.randint(1, 2)
+    a = random.randint(0, L-2)
+
+    data[a], data[a+gap] = data[a+gap], data[a]
+    return " ".join(data)
