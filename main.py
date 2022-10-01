289bd7bbfc9afbfe1263123d637069f5b80c2a64
Shenl <lshen30@jh.edu>
2022-10-01 02:58:57 -0400

    AAA

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ from performance import *
 
 parser = argparse.ArgumentParser(description='Take arguments from commandline')
 parser.add_argument('--mode', default="max", help='the mode to calculate flatness')
-parser.add_argument('--demo-shots', default=4, type=int, help='Type number of demos in the prompt if applicable')
+parser.add_argument('--demo-shots', default=16, type=int, help='Type number of demos in the prompt if applicable')
 parser.add_argument('--model', default='gpt2', type=str, help='model name')
 parser.add_argument('--batch-size', default=4, type=int, help='batch-size')
 parser.add_argument('--dataset', default='ag_news', type=str, help='dataset name')
-parser.add_argument('--seed', type=int, help='Type in seed that changes sampling of examples')
+parser.add_argument('--seed', default=42, type=int, help='Type in seed that changes sampling of examples')
 args = parser.parse_args()
 
 # from promptsource.templates import DatasetTemplates
@@ -28,20 +28,22 @@ name = args.model
 mode = args.mode
 
 np.random.seed(seed)
-torch.manual_seed(seed)
+#torch.manual_seed(seed)
 datasets = load_dataset(data)
 dict = {'0':'world','1':'sports','2':'business','3':'science'}
 demo_labels, demo_inputs, test_inputs, test_labels = create_set(datasets,demo_num,seed,dict)
 instruction1 = ''
-ag_news_prompts = ['What label best describes this news article?\n','What is this a piece of news regarding for?\n',' What is the category of the following news?\n', 'What is this news about?\n','Give the topic of the given text.\n','Read the text below, provide its focused topic.\n','Is this a piece of news regarding world, sport, business,or science?\n', 'Which section of a newspaper would this article likely appear in?\n']
+ag_news_prompts = ['What label best describes this news article?','What is this a piece of news regarding for?',' What is the category of the following news?', 'Which is the most relevant topic of the following news?','Give the topic of the given text.','Read the text below, provide its focused topic.','Is this a piece of news regarding world, sport, business,or science?', 'Which section of a newspaper would this article likely appear in?']
 config = GPT2Config.from_pretrained("gpt2", output_attentions=True)
-tokenizer = GPT2Tokenizer.from_pretrained(name)
+config.add_cross_attention
+tokenizer = GPT2Tokenizer.from_pretrained(name, pad_token='<|pad|>')
+#tokenizer1 = GPT2Tokenizer.from_pretrained(name)
 model = GPT2LMHeadModel.from_pretrained(name, config=config)
 # tokenizer.add_special_tokens({'pad_token': '[PAD]'})
 # model.resize_token_embeddings(len(tokenizer))
-test_dataset = loader_labeled(
-            test_inputs, test_labels, tokenizer, 1000, instruction1,model)
-
+#test_dataset = loader_labeled(
+            #test_inputs, test_labels, tokenizer, 1000, instruction1,model)
+model.resize_token_embeddings(len(tokenizer))
 model.eval().cuda()
 performance_all = []
 flatness_all = []
@@ -49,32 +51,35 @@ for prompt in ag_news_prompts:
     perturbed = set()
     instruction1 = prompt
     difference = []
-    ids = tokenizer.encode(instruction1, return_tensors='pt').to('cuda')
-    attention = model(ids,return_dict=True)['attentions'][0][-1].squeeze(0)
+    ids = tokenizer.encode(instruction1, return_tensors="pt",truncation=True,padding='max_length',max_length=30).to('cuda')
+    logits = model(ids,return_dict=True)
+    attention = logits['attentions'][0][-1].squeeze(0)
     while len(perturbed) < 10:
-        sss = pashuffle(instruction1, perc=10) + '\n'
-        if sss != prompt:
+        sss = pashuffle(instruction1, perc=10)
+        if sss+ '\n' != instruction1:
             perturbed.add(sss)
     for perturbed_sentence in perturbed:
-        ids = tokenizer.encode(perturbed_sentence, return_tensors='pt').to('cuda')
-        attention_p = model(ids, return_dict=True)['attentions'][0][-1].squeeze(0)
+        ids1 = tokenizer.encode(perturbed_sentence, return_tensors='pt',truncation=True,padding='max_length',max_length=30).to('cuda')
+        logits1 = model(ids1, return_dict=True)
+        attention_p =logits1['attentions'][0][-1].squeeze(0)
         difference.append(torch.abs(attention_p-attention))
     flatness = flat(difference,mode)
     flatness_all.append(flatness)
     for x,y in zip(demo_labels, demo_inputs):
-       instruction1 += '\n'+ "Input:" + y + '\n' + 'Output:' + dict[str(x)]
-
-    acc = score(instruction1,model,tokenizer,test_inputs, test_labels,batch_size)
+       instruction1 += '\n' + 'Input:' + y + '\n' + 'Output:' + dict[str(x)]
+    for i in range(len(test_inputs)):
+        test_inputs[i] = instruction1 + '\n' + 'Input:' + test_inputs[i] + '\n' + 'Output:'
+    acc = score(instruction1,model,tokenizer,test_inputs, test_labels,batch_size,prompt)
     performance_all.append(acc)
 
 
 from scipy import stats
 from scipy.stats import spearmanr, pearsonr, kendalltau
-a = (pearsonr(flatness, performance_all)[0])
-b = (spearmanr(flatness, performance_all)[0])
-c = (kendalltau(flatness, performance_all)[0])
-print('On {}, The pearson correlation between {} flatness and human score is {}'.format(mode, a))
-print('On {}, The spearman correlation between {} flatness and human score is {}'.format(mode, b))
-print('On {}, The kendall correlation between {} flatness and human score is {}'.format(mode, c))
+a = (pearsonr(flatness_all, performance_all)[0])
+b = (spearmanr(flatness_all, performance_all)[0])
+c = (kendalltau(flatness_all, performance_all)[0])
+print('On {}, The pearson correlation between {} flatness and human score is {}'.format(name,mode, a))
+print('On {}, The spearman correlation between {} flatness and human score is {}'.format(name,mode, b))
+print('On {}, The kendall correlation between {} flatness and human score is {}'.format(name,mode, c))
 
 
--- performance.py
+++ performance.py
@@ -2,9 +2,9 @@ from utils import *
 import torch.utils.data as Data
 from functools import reduce
 
-def score(candidate,model,tokenizer,test_inputs, test_labels,eval_batch_size):
+def score(candidate,model,tokenizer,test_inputs, test_labels,eval_batch_size,prompt):
     test_dataset = loader_labeled(
-        test_inputs[:20], test_labels[:20], tokenizer, 1000, candidate, model)
+        test_inputs[:20], test_labels[:20], tokenizer, 1000, candidate, model,prompt)
     test_loader = Data.DataLoader(
         dataset=test_dataset, batch_size=eval_batch_size, shuffle=False)
     acc_total = validate(test_loader, model)
@@ -16,16 +16,16 @@ def flat(input, mode = 'max'):
         avg_tensor = torch.mean(torch.stack(input))
         normalization = reduce(lambda x, y: x*y, list(avg_tensor.size()))
         mean = torch.sum(avg_tensor)/normalization
-        return mean.detach().cpu().numpy()
+        return mean.detach().cpu().item()
 
     elif mode == 'max':
         max_tensor = torch.max(torch.stack(input))
-        return max_tensor.detach().cpu().numpy()
+        return max_tensor.detach().cpu().item()
 
     elif mode == 'avg_max':
         avg_tensor = torch.mean(torch.stack(input))
         max_tensor = torch.max(avg_tensor)
-        return max_tensor.detach().cpu().numpy()
+        return max_tensor.detach().cpu().item()
 
 
 
