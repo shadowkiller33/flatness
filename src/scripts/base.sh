cd380b9898e3b9b13d754304cc3fb7a168362ad3
shadowkiller33 <2683142147@qq.com>
2022-10-05 20:41:39 -0400

    add some packages

--- src/scripts/base.sh
+++ src/scripts/base.sh
@@ -1,11 +1,17 @@
-proj_dir=/home/wtan12/flatness
-export PYTHONPATH="/home/wtan12/flatness"
-python $proj_dir/src/main.py \
-    --mode max \
-    --all_shots 8 \
-    --models gpt2 \
-    --bs 1 \
-    --datasets agnews \
-    --data-dir ${proj_dir}/data \
-    --num_seeds 42 \
-    --subsample_test_set 100
+#!/bin/bash
+#SBATCH -A danielk_gpu​
+#SBATCH --partition=a100​
+#SBATCH --gres=gpu:1​
+#SBATCH -N 1​
+#SBATCH --ntasks-per-node=12​
+
+
+source /etc/profile.d/modules.sh
+module load python
+
+python main.py --model gpt2   --dataset agnews   --all_shots 8 --num_seeds 1   --mode mean  --approx --data-dir /home/lshen30/flat/data
+#### execute code and write output file to OUT-24log.
+#time mpiexec ./code-mvapich.x > OUT-24log
+echo "Finished with job $SLURM_JOBID"
+
+#### mpiexec by default launches number of tasks requested
