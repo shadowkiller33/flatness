69dd697dcc11954d6857aedfc03204eacc0bc343
shadowkiller33 <2683142147@qq.com>
2022-10-11 12:28:33 -0400

    script renew

--- src/scripts/base.sh
+++ src/scripts/base.sh
@@ -4,19 +4,23 @@
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=12
-#SBATCH --output=gpt2xl-ag-%x.%j.out
+#SBATCH --output=ag-%x.%j.out
 #SBATCH --time=12:00:00
 module load anaconda
 conda info --envs
 source activate flat
 
 
-proj_dir=/home/lshen30/flat
-export PYTHONPATH="/home/lshen30/flat"
-python $proj_dir/src/main.py      --model gpt2-xl  --dataset agnews \
+user_id=lshen30
+proj_dir=/home/${user_id}/flat
+export PYTHONPATH="/home/${user_id}/flat"
+python -u $proj_dir/src/main.py  \
+    --model gpt2  --dataset agnews \
     --perturbed_num 7 --all_shots 4 \
-    --bs 4 --num_seeds 3  --subsample_test_set 1000 \
+    --bs 16 --num_seeds 3  --subsample_test_set 50 \
     --mode mean  --approx \
     --data-dir $proj_dir/data \
     --output-dir $proj_dir/output \
     --verbose
+
+
